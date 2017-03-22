60da1f126ab4b47092c2eb1768e8c80fb2870360
tuyentv <vat1906@gmail.com>
2017-03-22 23:04:34 +0700

    up
    up

--- main.go
+++ main.go
@@ -3,21 +3,21 @@ package main
 import (
 	"fmt"
 	mqttlib "./lib/mqtt"
-	//redis "./lib/redis/db"
-	//redisapi "./lib/redis/api"
+	redis "./lib/redis/db"
+	redisapi "./lib/redis/api"
 
 )
 
 
 func main() {
 	fmt.Print("Started server!!!\n")
-	/*
+
 	redis.InitRedisDb()
 	for i:=0;i<4000;i++ {
 		redisapi.SaveControlSignalExpire()
 		//redis.TestRedis()
 	}
-	*/
+
 
 	mqttlib.InitMqtt()
 
